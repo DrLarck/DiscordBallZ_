498b62e94f0bc680f7f3f9332e38ce4a6cd39758
Zyorhist <49216370+HaploZyorhist@users.noreply.github.com>
2020-02-03 13:54:30 -0700

    2 new characters, adjust dodge
    adjusted dodge chance on launch, added 2 more characters

---
+++ utility/cog/character/list/c013.py
@@ -0,0 +1,41 @@
+"""
+Represents Android 18
+
+--
+
+Author : Zyorhist
+
+Last update : 03/02/20 (DrLarck)
+"""
+
+# dependancies
+import asyncio
+
+# util
+from utility.cog.character.character import Character
+
+class Character_013(Character):
+    """
+    Represents the Android 18 unit from the Android saga.
+    """
+
+    def __init__(self):
+        Character.__init__(self)
+
+        # info
+        self.info.name = "Android 18"
+        self.info.id = 13
+        self.info.saga = "Android"
+
+        # stat
+        self.health.maximum = 2000
+        
+        self.damage.physical_max = 625
+        self.damage.ki_max = 775
+
+        self.defense.armor = 400
+        self.defense.spirit = 550
+        self.defense.dodge = 10
+
+        self.critical_chance = 30
+        self.regeneration.ki = 100
