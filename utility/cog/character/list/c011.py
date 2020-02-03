50356ce8b7bcb5d0d3606dcc6677dff16afa977b
Zyorhist <49216370+HaploZyorhist@users.noreply.github.com>
2020-02-03 13:47:59 -0700

    Update c011.py

---
+++ utility/cog/character/list/c011.py
@@ -0,0 +1,41 @@
+"""
+Represents Red Ribbon Soldier
+
+--
+
+Author : Zyorhist
+
+Last update : 03/02/20 (DrLarck)
+"""
+
+# dependancies
+import asyncio
+
+# util
+from utility.cog.character.character import Character
+
+class Character_011(Character):
+    """
+    Represents the Red Ribbon Soldier unit from the Red Ribbon Army saga.
+    """
+
+    def __init__(self):
+        Character.__init__(self) 
+
+        # info
+        self.info.name = "Red Ribbon Soldier"
+        self.info.id = 11
+        self.info.saga = "Red Ribbon Army"
+
+        # stat
+        self.health.maximum = 2000
+        
+        self.damage.physical_max = 775
+        self.damage.ki_max = 625
+
+        self.defense.armor = 550
+        self.defense.spirit = 400
+        self.defense.dodge = 5
+
+        self.critical_chance = 20
+        self.regeneration.ki = 5
