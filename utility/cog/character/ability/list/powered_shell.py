9217773e92084fe830872e604de0ac5e9e07bcc8
Belhadi Lahcene <lahcene.belhadi@gmail.com>
2020-02-18 16:12:05 +0100

    add powered shell ability

--- utility/cog/character/ability/list/powered_shell.py
+++ utility/cog/character/ability/list/powered_shell.py
@@ -12,8 +12,12 @@ Last update : 18/02/20 (DrLarck)
 import asyncio
 
 # util
+from utility.cog.character.ability.util.effect_checker import Effect_checker
 from utility.cog.character.ability.ability import Ability
 from utility.cog.displayer.move import Move_displayer
+
+# buff
+from utility.cog.character.ability.effect.buff.powered_shell import Buff_powered_shell
 
 class Powered_shell(Ability):
     """
@@ -44,4 +48,28 @@ class Powered_shell(Ability):
 
         # init
         move = Move_displayer()
+        checker = Effect_checker(self.caster)
+        powered_shell = Buff_powered_shell(self.client, self.ctx, self.caster, self.team_a, self.team_b)
+
+        # check if the caster already has the buff
+        # if the caster has the buff : reset duration
+        # else add the buff
+        has_buff = await checker.get_buff(powered_shell)
+
+        if(has_buff != None):  # has the buff
+            # reset duration
+            has_buff.duration = has_buff.initial_duration
         
+        else:  # doesn't have the buff
+            powered_shell.duration = powered_shell.initial_duration
+            self.caster.bonus.append(powered_shell)
+        
+        # setup move
+        _move = await move.get_new_move()
+
+        _move["name"] = self.name
+        _move["icon"] = self.icon
+
+        _move = await move.effect_move(_move)
+
+        return(_move)
