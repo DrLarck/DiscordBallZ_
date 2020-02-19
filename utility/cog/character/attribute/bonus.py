15d94feac42cbf23406c116da57851c2da011fad
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-02-19 14:04:45 +0000

    add `Bonus()` object

---
+++ utility/cog/character/attribute/bonus.py
@@ -0,0 +1,21 @@
+"""
+Bonus object
+
+--
+
+Author : DrLarck
+
+Last update : 19/02/20 (DrLarck)
+"""
+
+class Bonus():
+    """
+    Represents a bonus for the damage calculation
+
+    - Attribute 
+
+    `value` (`int`)
+    """
+
+    def __init__(self):
+        self.value = 0
